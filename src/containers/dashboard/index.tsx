a15328125e2a3d33e0f7fd2fccfb9527c8e3a007
love962965494 <962965494@qq.com>
2019-07-08 19:15:41 +0800

    test

--- src/containers/dashboard/index.tsx
+++ src/containers/dashboard/index.tsx
@@ -3,13 +3,15 @@ import Button from './components/button/Button'
 import Span from './components/span/Span'
 
 export default class Dashboard extends Component {
+  const test = 123
+
   public render() {
     return (
       <div>
-           我是首页
-      <Span />
+        我是首页
+        <Span />
     <Button />
-  </div>
+      </div>
     )
   }
 }
