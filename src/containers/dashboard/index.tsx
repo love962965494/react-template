8a86dcb9b06e086a56ddd0292572ad9d11c63670
love962965494 <962965494@qq.com>
2019-07-08 19:23:07 +0800

    test

--- src/containers/dashboard/index.tsx
+++ src/containers/dashboard/index.tsx
@@ -1,17 +1,15 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Button from './components/button/Button'
 import Span from './components/span/Span'
 
-export default class Dashboard extends Component {
-  const test = 123
+export default function index() {
+  const test = 23
 
-  public render() {
-    return (
-      <div>
-        我是首页
-        <Span />
-    <Button />
-      </div>
-    )
-  }
+  return (
+    <div>
+      我是首页
+<Button/>
+         <Span/>
+    </div>
+  )
 }
